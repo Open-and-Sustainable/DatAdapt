c2b6a631d999e2b648bda2b4f4099967a3110699
Riccardo Boero <ribo@nilu.no>
2024-08-06 16:02:28 +0200

    Added damage data fetching

--- src/DatAdapt.jl
+++ src/DatAdapt.jl
@@ -6,10 +6,12 @@ using DataFrames
 # Include and use the renamed modules
 include("DatabaseAccess.jl")
 include("ExposureDataFetch.jl")
+include("DamageDataFetch.jl")
 #include("DataCleaning.jl")
 
 using .DatabaseAccess
 using .ExposureDataFetch
+using .DamageDataFetch
 
 #using .DataCleaning
 
@@ -19,9 +21,15 @@ export fetch_exposure_data
 function fetch_exposure_data()
     # wb_test_data = DataFetch.fetch_WB_test_data()
     # write_duckdb_table(wb_test_data, db_path, "wb_test_data")
-    db_path = "data/raw/DatAdapt_1990-2021.duckdb"
+    db_path = "data/raw/DatAdapt_1980-2021.duckdb"
     E_data = ExposureDataFetch.fetch_exposure_data(1980, 2021)
     DatabaseAccess.write_duckdb_table!(E_data, db_path, "exposure")
+end
+
+function fetch_damage_data()
+    db_path = "data/raw/DatAdapt_1980-2021.duckdb"
+    D_data = ExposureDataFetch.fetch_damage_data(1980, 2021)
+    DatabaseAccess.write_duckdb_table!(D_data, db_path, "damage")
 end
 
 function clean_data(df::DataFrame)
