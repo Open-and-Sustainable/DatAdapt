6908df9ca3a7a36b883814918bd892cebddaf8ca
Riccardo Boero <ribo@nilu.no>
2024-08-07 09:40:00 +0200

    added logging to exposure fetching

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the Julia script and call the fetch_exposure_data function
-julia --project=. -e 'push!(LOAD_PATH, "src"); using DatAdapt; DatAdapt.fetch_exposure_data()'
+julia --project=. -e 'push!(LOAD_PATH, "src"); using DatAdapt; DatAdapt.fetch_exposure_data()' 2>&1 | tee exposure.log
 
 # Run the Julia script and call the fetch_damage_data function
 #julia --project=. -e 'push!(LOAD_PATH, "src"); using DatAdapt; DatAdapt.fetch_damage_data()'
