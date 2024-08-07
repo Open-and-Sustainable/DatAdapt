d08de74bffd4b4dab02dbbb2f29cae255a50dcfa
Riccardo Boero <ribo@nilu.no>
2024-08-07 09:16:57 +0200

    work on shell script

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the Julia script and call the fetch_exposure_data function
-#julia --project=. -e 'push!(LOAD_PATH, "src"); using DatAdapt; DatAdapt.fetch_exposure_data()'
+julia --project=. -e 'push!(LOAD_PATH, "src"); using DatAdapt; DatAdapt.fetch_exposure_data()'
 
 # Run the Julia script and call the fetch_damage_data function
-julia --project=. -e 'push!(LOAD_PATH, "src"); using DatAdapt; DatAdapt.fetch_damage_data()'
+#julia --project=. -e 'push!(LOAD_PATH, "src"); using DatAdapt; DatAdapt.fetch_damage_data()'
